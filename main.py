5caa5b253d212b3b2c9c98ee352d4e0532227531
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-06-02 21:02:16 +0200

    Created quantum helloworld.

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
+# Qiskit import
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, QiskitError
 from qiskit_ibm_provider import IBMProvider
+from qiskit.visualization import circuit_drawer
 
+# Load environment variables
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-# IBMProvider.save_account(token=os.getenv("IBMQ_API_KEY"))
+imageFolder = "images/"
+# Load IBM Quantum Experience provider
 provider = IBMProvider(token=os.getenv("IBMQ_API_KEY"))
-print(provider.backends())
+
+def saveQuantumCircuit(qc: QuantumCircuit, name: str):
+    qc.draw(output='mpl', filename=imageFolder + name + '.png')
+    return name + '.png'
+
+def quantumHelloWorld():
+    # Create a Quantum Circuit acting on the 2 register
+    qc = QuantumCircuit(2)
+
+    # Add a H gate on qubit 0
+    qc.h(0)
+
+    # Add a CX (CNOT) gate on control qubit 0 and target qubit 1
+    qc.cx(0, 1)
+
+    saveQuantumCircuit(qc, 'quantum_hello_world')
+    return qc
+
+def getBackend():
+    i = int(input())
+    return provider.get_backend(provider.backends()[i])
+
+
